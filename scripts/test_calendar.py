dbd1607f67c03ea2e4f7c516df35df68a1c00e76
jjchen762 <88948788+jjchen762@users.noreply.github.com>
2023-03-10 17:22:00 -0800

    Add a script to tes calendar

---
+++ scripts/test_calendar.py
@@ -0,0 +1,9 @@
+import pandas_market_calendars as mcal
+
+# Create a calendar
+nyse = mcal.get_calendar('NYSE')
+
+# Show available calendars
+print(mcal.get_calendar_names())
+early = nyse.schedule(start_date='2012-07-01', end_date='2012-07-10')
+print(early)
