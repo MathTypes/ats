a8c37fbd5adadb34eed51e44dffa44346e6622b0
jjchen762 <88948788+jjchen762@users.noreply.github.com>
2023-03-24 18:21:17 -0700

    add more neo4j streamlit apps

---
+++ src/data/promptier.py
@@ -0,0 +1,29 @@
+
+import os
+from promptify import OpenAI
+from promptify import Prompter
+
+import argparse
+import logging
+
+parser = argparse.ArgumentParser(
+    description='A script to add GPT annotation'
+)
+parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                    action="store_true")
+parser.add_argument("--input", type=str, required=True)
+
+args = parser.parse_args()
+if args.verbose:
+    logging.basicConfig(level=logging.DEBUG)
+
+api_key = os.environ["OPENAI_API_KEY"]
+model = OpenAI(api_key)  # or `HubModel()` for Huggingface-based inference
+nlp_prompter = Prompter(model)
+
+result = nlp_prompter.fit('ner.jinja',
+                          domain='finance',
+                          text_input=args.input,
+                          labels=None)
+# Output
+print(result)
