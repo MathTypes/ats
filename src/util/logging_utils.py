6cb1b1c791546b10232a186f19e496948cbb2004
jjchen762 <88948788+jjchen762@users.noreply.github.com>
2023-04-02 09:26:15 -0700

    Add module name to logging

--- src/util/logging_utils.py
+++ src/util/logging_utils.py
@@ -2,7 +2,7 @@ import logging
 import sys
 
 def init_logging():
-    logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+    logging.basicConfig(format='%(asctime)s,%(msecs)03d %(levelname)-8s %(name)s[%(module)s:%(filename)s:%(lineno)d] %(message)s',
         datefmt='%Y-%m-%d:%H:%M:%S',
         level=logging.INFO)
 
