2214478daa1dd3eb27cca52c672a2993edd83925
jjchen762 <88948788+jjchen762@users.noreply.github.com>
2023-04-04 19:01:57 -0700

    Add driver for neo4j

---
+++ src/neo4j_util/driver.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+import logging
+from neo4j import GraphDatabase
+
+from util import config_utils
+
+USER = "neo4j"
+
+def get_driver():
+    args = config_utils.get_args()
+    host = config_utils.get_neo4j_host()
+    password = config_utils.get_neo4j_password()
+    driver = GraphDatabase.driver(host, auth=(USER, password))
+    return driver
