0b585ffacc29464651349a7aa5902c98675ef8cb
jjchen762 <88948788+jjchen762@users.noreply.github.com>
2023-04-14 07:42:18 -0400

    Update neo4j host metadata

--- src/twitter/download_tweet_by_user.sh
+++ src/twitter/download_tweet_by_user.sh
@@ -1,1 +1,1 @@
-PYTHONPATH=. python3 twitter/download_tweet.py --username=$3 --since=$1 --until=$2 -v  --neo4j_host=bolt://34.31.6.22:7687 --neo4j_pass=Tq7ks8zY
+PYTHONPATH=. python3 twitter/download_tweet.py --username=$3 --since=$1 --until=$2 -v  --neo4j_host=bolt://34.105.8.79:7687 --neo4j_pass=Tq7ks8zY
