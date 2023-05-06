16da17ca406e072828c8cbf1d06bf910399832e0
santon762 <92042293+santon762@users.noreply.github.com>
2023-05-06 09:22:24 -0700

    Clean up code

--- src/model/write_parquet.py
+++ src/model/write_parquet.py
@@ -1,54 +1,27 @@
 # Usage:
 #   PYTHONPATH=.. python3 write_parquet.py ES FUT
 #
-import os
 from datetime import datetime, timezone, timedelta
-from typing import List
-
-import pandas as pd
 import logging
-import ray
-from pyarrow import csv
-import yfinance as yf
+import os
+from typing import List
+import sys
+
 import numpy as np
+import pandas as pd
+from pyarrow import csv
 import pytz
+import pyarrow as pa
+import ray
+from ray.util.dask import enable_dask_on_ray
 
 from settings.default import PINNACLE_DATA_CUT, PINNACLE_DATA_FOLDER
 
 def pull_sample_data(ticker: str, intraday: bool) -> pd.DataFrame:
     return pull_futures_sample_data(ticker)
 
-def hloc(c):
-    if c.name == "open":
-        return c.iloc[0]
-    if c.name == "close":
-        return c.iloc[-1]
-    if c.name == "high":
-        return c.max()
-    if c.name == "low":
-        return c.min()
-    if c.name == "volume":
-        return c.sum()
-    if c.name == "date":
-        return c.iloc[0]
-    return c    
-
-def ceil_dt(tm):
-    tm = tm - timedelta(minutes=tm.minute % 30,
-                        seconds=tm.second,
-                        microseconds=tm.microsecond)
-    return tm
-
-# UDF as a function on Pandas DataFrame batches.
-def pandas_transform(df: pd.DataFrame) -> pd.DataFrame:
-    eastern = pytz.timezone('US/Eastern')
-    custom_date_parser = lambda x: x.tz_localize(eastern)
-    df.Time = df.Time.apply(custom_date_parser)
-    df["Date"] = df.Time.apply(lambda x : ceil_dt(x))
-    return df
-
 def pull_futures_sample_data(ticker: str, asset_type: str) -> pd.DataFrame:
-    ticker = ticker.replace("CME_","")
+    #ticker = ticker.replace("CME_","")
     names = ["Time", "Open", "High", "Low", "Close", "Volume"]
     if asset_type in ["FUT"]:
         file_path = os.path.join("/Users/alex/git_repo/ats/src/model/futures",
@@ -60,39 +33,26 @@ def pull_futures_sample_data(ticker: str, asset_type: str) -> pd.DataFrame:
                column_names=names,
                skip_rows=1)
     parse_options = csv.ParseOptions(delimiter=",")
-    #convert_options = csv.ConvertOptions(timestamp_parsers=["%Y-%m-%d %H:%M:%S"])
     ds = ray.data.read_csv(file_path,
-                           #convert_options=convert_options,
                            parse_options=parse_options, read_options=read_options)
-    ds = ds.repartition(100).map_batches(pandas_transform, batch_size=1000)
     ds = ds.sort("Time")
-    ds = ds.groupby("Date")
-    ds = ds.map_groups(lambda g: g.apply(lambda c: [hloc(c)]))
-    #ds = ds.add_column("ticker", lambda r: ticker)
     return ds
 
-import sys
-import ray
-from ray.util.dask import enable_dask_on_ray
-import pyarrow as pa
-import pickle
 
 ticker = sys.argv[1]
 asset_type = sys.argv[2]
 ray.init()  # Start or connect to Ray.
 enable_dask_on_ray()  # Enable the Ray scheduler backend for Dask.
+
 ds = pull_futures_sample_data(ticker, asset_type)
 df = ds.to_dask()
-df = df.drop(columns=["Time"])
-file_path = os.path.join("/Users/alex/git_repo/ats/src/model/data", f"{ticker}_30min.pkl")
-schema = pa.schema([pa.field('Date', pa.list_(pa.timestamp('ms'))),
-                    pa.field('Open', pa.list_(pa.float64())),
-                    pa.field('Close', pa.list_(pa.float64())),
-                    pa.field('High', pa.list_(pa.float64())),
-                    pa.field('Low', pa.list_(pa.float64())),
-                    pa.field('Volume', pa.list_(pa.float64())),
-                    ])
-df.to_parquet(file_path, schema=schema)
-#with open(file_path, 'wb') as handle:
-#    pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
-#df.to_pickle()
+df = df.set_index('Time')
+df = df.resample('30Min').agg({'Open': 'first', 
+                             'High': 'max', 
+                             'Low': 'min', 
+                             'Close': 'last',
+                             'Volume': 'sum'})
+logging.info(f"df_schema:{df.info()}")
+logging.info(f"df_schema:{df.info()}")
+file_path = os.path.join("/Users/alex/git_repo/ats/src/model/data", asset_type, "30min", ticker)
+df.to_parquet(file_path, engine='fastparquet')
