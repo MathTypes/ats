a099d7519116750edb72877a68572c417eb897c2
santon762 <92042293+santon762@users.noreply.github.com>
2023-05-09 21:42:31 -0700

    Squeeze last dimension from y

--- src/model/timeseries_transformer.py
+++ src/model/timeseries_transformer.py
@@ -233,8 +233,8 @@ class TimeSeriesTFT(pl.LightningModule):
         return decoder_output
     
     def compute_loss(self, y_hat, y):
+        y = torch.squeeze(y[:,:,4], -1)
         logging.info(f"y_hat:{y_hat.shape}, y:{y.shape}")
-        y = y[:,:,4].squeeze(-1)
         loss = self.criterion(y_hat, y)
         return loss
 
