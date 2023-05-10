c68234d0889f0e535a201110fbb1c708eadd2912
santon762 <92042293+santon762@users.noreply.github.com>
2023-05-09 22:28:08 -0700

    Put prediction on GPU

--- src/model/inference.py
+++ src/model/inference.py
@@ -126,4 +126,4 @@ def run_encoder_decoder_inference(
     # Make final prediction
     final_prediction = model.to('cuda')((src, tgt, src_mask, tgt_mask))
 
-    return final_prediction
+    return final_prediction.to('cuda')
