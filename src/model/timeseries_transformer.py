10be192506b79e534023a2c671e88296938ac2a2
santon762 <92042293+santon762@users.noreply.github.com>
2023-05-09 22:42:10 -0700

    Put prediction on GPU

--- src/model/timeseries_transformer.py
+++ src/model/timeseries_transformer.py
@@ -184,7 +184,8 @@ class TimeSeriesTFT(pl.LightningModule):
             tgt_mask: the mask for the tgt sequence to prevent the model from
                       using data points from the target sequence
         """
-        (src, tgt, src_mask, tgt_mask) = X.to('cuda') 
+        (src, tgt, src_mask, tgt_mask) = X
+        src = src.to('cuda')
         #print("From model.forward(): Size of src as given to forward(): {}".format(src.size()))
         #print("From model.forward(): tgt size = {}".format(tgt.size()))
 
