3bbe688c138366da99ca6a825123d3e9ae4740a3
jjchen762 <88948788+jjchen762@users.noreply.github.com>
2023-05-11 16:23:02 -0700

    Add batch size to dataset module

--- src/model/models.py
+++ src/model/models.py
@@ -59,8 +59,8 @@ class AttentionEmbeddingLSTM(pl.LightningModule):
     
     def training_step(self, batch, batch_idx):
         x, y = batch
-        x = x[:,1:,...]
-        y = y[:,1:,...]
+        x = x.permute(0, 2, 1)
+        y = x.permute(0, 2, 1)
         logging.info(f"x:{x.shape}")
         logging.info(f"y:{y.shape}")
         y_hat = self.forward(x)
@@ -71,8 +71,8 @@ class AttentionEmbeddingLSTM(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
-        x = x[:,1:,...]
-        y = y[:,1:,...]
+        x = x.permute(0, 2, 1)
+        y = x.permute(0, 2, 1)
         logging.info(f"x:{x.shape}")
         logging.info(f"y:{y.shape}")
         y_hat = self.forward(x)
@@ -82,8 +82,8 @@ class AttentionEmbeddingLSTM(pl.LightningModule):
 
     def test_step(self, batch, batch_idx):
         x, y = batch
-        x = x[:,1:,...]
-        y = y[:,1:,...]
+        x = x.permute(0, 2, 1)
+        y = x.permute(0, 2, 1)
         y_hat = self.forward(x)
         loss = self.compute_loss(y_hat, y)
         self.log('test_loss', loss)
