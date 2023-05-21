c6bd3fe99ab423459086c318a6fb1cdeb2f9458a
jjchen762 <88948788+jjchen762@users.noreply.github.com>
2023-05-20 17:17:59 -0700

    Add flutter

--- src/ib-trading/gke/ib-gateway/ibc/cmd.sh
+++ src/ib-trading/gke/ib-gateway/ibc/cmd.sh
@@ -5,7 +5,7 @@ JAVA_PATH=${JAVA_PATH_ROOT}/$(ls "${JAVA_PATH_ROOT}")/${JAVA_VRSN}/bin
 export JAVA_PATH
 #TWS_MAJOR_VRSN=$(grep -Eo "IB Gateway [0-9]{3}" < "${TWS_INSTALL_LOG}" | head -n 1 | cut -d" " -f3)
 #export TWS_MAJOR_VRSN
-
+rm /tmp/.X0-lock
 echo "Starting Xvfb..."
 Xvfb "$DISPLAY" -ac -screen 0 1024x768x16 +extension RANDR &
 sleep 1
