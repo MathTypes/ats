4f16381321f68662d5e3653f3cc03a0a7764b5a5
root <jianjun00@gmail.com>
2023-06-04 22:22:26 +0000

    Add pct change to rolling ts

--- src/model/write_rolling_ts.py
+++ src/model/write_rolling_ts.py
@@ -20,6 +20,14 @@ from util import time_util
 
 from tsfresh.utilities.dataframe_functions import roll_time_series
 from settings.default import PINNACLE_DATA_CUT, PINNACLE_DATA_FOLDER
+import re
+
+def get_id_time(id: str):
+    result = re.match(r'.*(\d\d\d\d\-\d\d\-\d\d\s+\d\d:\d\d:\d\d).*', str(id))
+    if result:
+        return datetime.datetime.strptime(result.group(1), "%Y-%m-%d %H:%M:%S")
+    logging.info(f"can not extract id from {id}")
+    return None
 
 def pull_sample_data(ticker: str, intraday: bool) -> pd.DataFrame:
     return pull_futures_sample_data(ticker)
@@ -38,9 +46,10 @@ def pull_futures_sample_data(ticker: str, asset_type: str, start_date, end_date,
     ds = ray.data.read_csv(file_path,
                            parse_options=parse_options, read_options=read_options)
     ds = ds.sort("Time")
-    return ds
+    return ds.repartition(100)
 
 if __name__ == '__main__':
+    pd.set_option('display.max_columns', None)
     parser = config_utils.get_arg_parser("Scrape tweet by id")
     parser.add_argument("--input_dir", type=str)
     parser.add_argument("--output_dir", type=str)
@@ -68,7 +77,7 @@ if __name__ == '__main__':
 
     for cur_date in time_util.monthlist(args.start_date, args.end_date):
         for_date = cur_date[0]
-        since = cur_date[0] + datetime.timedelta(days=-50)
+        since = cur_date[0] + datetime.timedelta(days=-50*5)
         until = cur_date[1] + datetime.timedelta(days=5)
             
         ds = pull_futures_sample_data(ticker, asset_type, since, until, args.input_dir)
@@ -85,7 +94,6 @@ if __name__ == '__main__':
                                         'volume': 'sum',
                                         "dv": 'sum'})
         df = df.compute()
-        df = df.dropna()
         df["ticker"] = ticker
         df["time"] = df.index
         df["cum_volume"]  = df.volume.cumsum()
@@ -93,7 +101,7 @@ if __name__ == '__main__':
         logging.info(f"df:{df.head()}")
         df_pct_back = df[["close", "volume", "dv"]].pct_change(periods=1)
         df_pct_forward = df[["close", "volume", "dv"]].pct_change(periods=-1)
-        df = df.join(df_pct_back).join(df_pct_forward)
+        df = df.join(df_pct_back, rsuffix='_back').join(df_pct_forward, rsuffix='_fwd')
         logging.info(f"df:{df.head()}")
         #df = roll_time_series(df, column_id="ticker", column_sort="time")
         id_column = "ticker"
@@ -104,7 +112,7 @@ if __name__ == '__main__':
                 column_kind=None,
                 rolling_direction=1,
                 max_timeshift=13*(50*4+3),
-                min_timeshift=13*(50*4+3))
+                min_timeshift=13*(50+3))
         df["id"] = df["id"].astype(str)
         df["month"] = df.time.dt.month  # categories have be strings
         df["year"] = df.time.dt.year  # categories have be strings
@@ -115,14 +123,17 @@ if __name__ == '__main__':
         df["week_of_year"] = df.time.apply(lambda x:x.isocalendar().week)
         #df["date"] = df.est_time
         df["time_idx"] = df.index
+        df["id_time"] = df["id"].apply(get_id_time)
+        df["id_hour_of_day"] = df.id_time.apply(lambda x:x.hour)
+        df["id_day_of_month"] = df.id_time.apply(lambda x:x.day)
         df = df.dropna()
         logging.info(f"df:{df.describe()}")
-        df = df[df.hour_of_day.isin([3]) & df.day_of_month.isin(0)]
+        df = df[(df.id_hour_of_day.isin([10, 15])) & (df.id_day_of_month.isin([1]))]
         logging.info(f"df_schema:{df.info()}")
         logging.info(f"df:{df.head()}")
         file_path = os.path.join(args.output_dir, asset_type, "30min_rsp", ticker)
         if not os.path.exists(file_path):
             os.makedirs(file_path)
-        ds = ray.data.from_pandas(df).repartition(100)
+        ds = ray.data.from_pandas(df).repartition(10)
         ds.write_parquet(file_path + "/" + for_date.strftime("%Y%m%d"))
     ray.shutdown()
