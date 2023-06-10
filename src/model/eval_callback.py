1a54ebc8faa5fa8a7545ca9f869d5a5a7a682d11
Jianjun Chen <jianjun00@gmail.com>
2023-06-10 19:54:59 +0000

    Add callback to log eval dataset with prediction

---
+++ src/model/eval_callback.py
@@ -0,0 +1,47 @@
+from wandb.keras import WandbEvalCallback
+
+class WandbClfEvalCallback(WandbEvalCallback):
+    def __init__(
+        self, validloader, data_table_columns, pred_table_columns, num_samples=100
+    ):
+        super().__init__(data_table_columns, pred_table_columns)
+        self.val_data = next(iter(validloader))
+
+    def add_ground_truth(self, logs=None):
+        for idx, (x, y) in enumerate(self.val_data):
+            self.data_table.add_data(
+                idx,
+                x["time"],
+                x["close_pct"],
+                #wandb.Image(image),
+                #np.argmax(label, axis=-1)
+                y[0]
+            )
+
+    def add_model_predictions(self, epoch, logs=None):
+        # Get predictions
+        preds = self._inference()
+        table_idxs = self.data_table_ref.get_index()
+
+        for idx in table_idxs:
+            pred = preds[idx]
+            self.pred_table.add_data(
+                epoch,
+                self.data_table_ref.data[idx][0],
+                self.data_table_ref.data[idx][1],
+                self.data_table_ref.data[idx][2],
+                pred
+            )
+
+    def _inference(self):
+      preds = []
+      for x, y in self.val_data:
+          logging.info(f"x:{x}")
+          logging.info(f"y:{y}")
+          pred = self.model(x)
+          logging.info(f"pred:{pred}")
+          argmax_pred = tf.argmax(pred, axis=-1).numpy()[0]
+          preds.append(argmax_pred)
+
+      return preds
+     
