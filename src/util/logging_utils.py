deb6732c24d705583637ebbdcd36650d3c444514
Jianjun Chen <jianjun00@gmail.com>
2023-06-27 22:56:53 +0000

    Set log config from file

--- src/util/logging_utils.py
+++ src/util/logging_utils.py
@@ -1,8 +1,13 @@
 import logging
+from os import path
 import sys
+import logging.config
 
 
 def init_logging():
+    #log_file_path = path.join(path.dirname(path.abspath(__file__)), '../util/logging.conf')
+    #logging.info(f"log_file_path:{log_file_path}")
+    #logging.config.fileConfig(log_file_path)
     logging.basicConfig(
         format="%(asctime)s,%(msecs)03d %(levelname)-8s %(name)s[%(module)s:%(filename)s:%(lineno)d] %(message)s",
         datefmt="%Y-%m-%d:%H:%M:%S",
@@ -12,9 +17,12 @@ def init_logging():
     root = logging.getLogger()
     root.setLevel(logging.ERROR)
 
+    #ch = logging.StreamHandler(sys.stdout)
     ch = logging.StreamHandler(sys.stdout)
     ch.setLevel(logging.INFO)
     FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
     formatter = logging.Formatter(FORMAT)
     ch.setFormatter(formatter)
     root.addHandler(ch)
+    logging.info(f"init")
+    #exit(0)
