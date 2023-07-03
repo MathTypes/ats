Add OHLC chart for raw price and fix the issue where y_quantiles are accumulated in wrong dim

src/model/eval_callback.py
@@ -1,24 +1,26 @@
 from io import BytesIO
-from typing import Any, Callable, Dict, Iterable, List, Literal, Optional, Tuple, Union
 import logging
+from typing import Any, Callable, Dict, Iterable, List, Literal, Optional, Tuple, Union
+
 from omegaconf import OmegaConf
-import PIL
 import pandas as pd
-from typing import Any, Dict, Optional, Set
-import torch
-from wandb.keras import WandbEvalCallback
-import wandb
+import PIL
+import plotly.graph_objects as go
 from lightning.pytorch.callbacks.callback import Callback
 from lightning.pytorch.utilities.types import STEP_OUTPUT
+from plotly.subplots import make_subplots
 from pytorch_forecasting.utils import create_mask, detach, to_list
+import torch
+import wandb
+from wandb.keras import WandbEvalCallback
 
 class WandbClfEvalCallback(WandbEvalCallback, Callback):
     def __init__(
             self, data_module, target, num_samples=10, every_n_epochs=1
     ):
-        super().__init__(["ticker", "time", "time_idx", "day_of_week", "hour_of_day", "year", "month", "day_of_month",
+        super().__init__(["ticker", "time", "time_idx", "day_of_week", "hour_of_day", "year", "month", "day_of_month", "price_img",
                           "act_close_pct_max", "act_close_pct_min", "close_back_cumsum"],
-                         ["ticker", "time", "time_idx", "day_of_week", "hour_of_day", "year", "month", "day_of_month",
+                         ["ticker", "time", "time_idx", "day_of_week", "hour_of_day", "year", "month", "day_of_month", "price_img",
                           "act_close_pct_max", "act_close_pct_min", "close_back_cumsum",
                           "pred_time_idx", "pred_close_pct_max", "pred_close_pct_min", "img", "error_max", "error_min", "rmse", "mae"])
         self.val_x_batch = []
@@ -96,15 +98,21 @@ class WandbClfEvalCallback(WandbEvalCallback, Callback):
             indices = self.indices_batch[batch_idx]
             #logging.info(f"val_x.encoder_target.shape:{len(val_x['encoder_target'])}, encoder_target[0].shape:{val_x['encoder_target'][0].shape}")
             #logging.info(f"val_y:{val_y}")
-            y_close_cum_sum = val_y[0]
+            y_close = val_y[0]
             # TODO: fix following hack to deal with multiple targets
             #logging.info(f"y_close_cum_sum:{type(y_close_cum_sum)}")
-            if isinstance(y_close_cum_sum, list):
+            if isinstance(y_close, list):
                 #logging.info("y_close_cum_sum is list")
-                y_close_cum_sum = y_close_cum_sum[0]
+                y_close = y_closem[0]
+            #logging.info(f"y_close_cum_sum before:{y_close_cum_sum}")
+            y_close_cum_sum = torch.cumsum(y_close, dim=-1)
+            top_idx = torch.topk(y_close_cum_sum, 5)
             #logging.info(f'y_close_cum_sum:{y_close_cum_sum.shape}, len:{len(y_close_cum_sum)}')
-            #logging.info(f"y_close_cum_sum:{y_close_cum_sum}, len:{len(y_close_cum_sum)}, shape:{y_close_cum_sum.shape}")
+            #logging.info(f"top_idx:{top_idx}")
+            #logging.info(f"y_close_cum_sum max:{torch.max(y_close_cum_sum)}, len:{len(y_close_cum_sum)}, shape:{y_close_cum_sum.shape}")
             #logging.info(f"encoder_target:{val_x['encoder_target'].shape}")
+            #logging.info(f"val_x:{val_x}")
+            #exit(0)
             for idx in range(len(y_close_cum_sum)):
               # TODO: fix [0] hack to deal with multiple target
               if self.target_size > 1:
@@ -114,6 +122,8 @@ class WandbClfEvalCallback(WandbEvalCallback, Callback):
                     base = val_x['encoder_target'][idx][-1]
                   else:
                     base = val_x['encoder_target'][0][idx][-1]
+              # when using close_back, base is always 0
+              base = 0
               #logging.info(f"idx:{idx}, y_close_cum_sum:{y_close_cum_sum}")
               #logging.info(f"encoder_x:{self.val_x['encoder_x'][idx]}")
               #logging.info(f"encoder_target:{self.val_x['encoder_target'][idx]}")
@@ -122,10 +132,31 @@ class WandbClfEvalCallback(WandbEvalCallback, Callback):
               #logging.info(f"index:{index}")
               train_data_row = self.matched_eval_data[self.matched_eval_data.time_idx == index.time_idx].iloc[0]
               #logging.info(f"train_data_row:{train_data_row}")
+              #exit(0)
               dm = train_data_row["time"]
               #logging.info(f"dm:{dir(dm)}")
               y_close_cum_sum_row = y_close_cum_sum[idx]
-              #logging.info(f"y_close_cum_sum_row:{y_close_cum_sum_row}")
+              train_data_rows = self.matched_eval_data[(self.matched_eval_data.time_idx>=index.time_idx-320) & (self.matched_eval_data.time_idx<index.time_idx+32)]
+              fig = go.Figure(data=go.Ohlc(x=train_data_rows['time'],
+                    open=train_data_rows['open'],
+                    high=train_data_rows['high'],
+                    low=train_data_rows['low'],
+                    close=train_data_rows['close']))
+              fig.update(layout_xaxis_rangeslider_visible=False)
+              fig.update_xaxes(
+                  rangebreaks=[
+                      dict(bounds=["sat", "mon"]), #hide weekends
+                      #dict(bounds=[17, 9], pattern="hour"), #hide hours outside of 9am-5pm
+                      #dict(values=["2015-12-25", "2016-01-01"])  # hide Christmas and New Year's
+                  ]
+              )
+              img_bytes = fig.to_image(format="png") # kaleido library
+              im = PIL.Image.open(BytesIO(img_bytes))
+              if torch.max(y_close_cum_sum_row) > 0.10:
+                  logging.info(f"bad row:{train_data_row}, idx:{idx}, index:{index}")
+                  logging.info(f"y_close_cum_sum_row:{y_close_cum_sum_row}")
+                  logging.info(f"y_close:{y_close[idx]}")
+                  logging.info(f"train_data_rows:{train_data_rows}")
               self.data_table.add_data(
                 train_data_row["ticker"], # 0 ticker
                 dm, # 1 time
@@ -135,10 +166,10 @@ class WandbClfEvalCallback(WandbEvalCallback, Callback):
                 train_data_row["year"], # 5 year
                 train_data_row["month"], # 6 month
                 train_data_row["day_of_month"], # 7 day_of_month
-                #wandb.Image(image),
+                wandb.Image(im),  # image
                 #np.argmax(label, axis=-1)
-                torch.max(y_close_cum_sum_row) - base, # 8 max
-                torch.min(y_close_cum_sum_row) - base, # 9 min
+                torch.max(y_close_cum_sum_row) - base, # 9 max
+                torch.min(y_close_cum_sum_row) - base, # 10 min
                 base, # 10 close_back_cusum
               )
         #logging.info(f"self.data_table:{self.data_table}")
@@ -163,15 +194,16 @@ class WandbClfEvalCallback(WandbEvalCallback, Callback):
                 self.data_table_ref.data[idx][5], # year
                 self.data_table_ref.data[idx][6], # month
                 self.data_table_ref.data[idx][7], # day_of_month
-                self.data_table_ref.data[idx][8], # act_max_close_pct
-                self.data_table_ref.data[idx][9], # act_min_close_pct
-                self.data_table_ref.data[idx][10], # close_back_cumsum
+                self.data_table_ref.data[idx][8], # price image
+                self.data_table_ref.data[idx][9], # act_max_close_pct
+                self.data_table_ref.data[idx][10], # act_min_close_pct
+                self.data_table_ref.data[idx][11], # close_back_cumsum
                 pred[0], # pred_time_idx
                 pred[1] - self.data_table_ref.data[idx][10], # pred_close_pct_max
                 pred[2] - self.data_table_ref.data[idx][10], # pred_close_pct_min
                 pred[3], # img
-                pred[1] - self.data_table_ref.data[idx][10] - self.data_table_ref.data[idx][8], # error_max
-                pred[2] - self.data_table_ref.data[idx][10] - self.data_table_ref.data[idx][9], # error_min
+                pred[1] - self.data_table_ref.data[idx][10] - self.data_table_ref.data[idx][9], # error_max
+                pred[2] - self.data_table_ref.data[idx][10] - self.data_table_ref.data[idx][10], # error_min
                 #pred[4], # img_interp
                 pred[4], # rmse
                 pred[5], # mae
@@ -210,24 +242,30 @@ class WandbClfEvalCallback(WandbEvalCallback, Callback):
         prediction_kwargs = {}
         quantiles_kwargs = {}
         y_hats = to_list(self.pl_module.to_prediction(out, **prediction_kwargs))[0]
+        y_hats_cum = torch.cumsum(y_hats, dim=-1)
         y_quantiles = to_list(self.pl_module.to_quantiles(out, **quantiles_kwargs))[0]
         #logging.info(f"y_quantiles:{y_quantiles}")
         #logging.info(f"y_hats:{y_hats}")
         #exit(0)
         #logging.info(f"y_quantiles:{y_quantiles.shape}")
         for idx in range(len(y_hats)):
-          #logging.info(f"self.data_table_ref.data[idx]:{self.data_table_ref.data[idx]}")
+          decoder_time_idx = x["decoder_time_idx"][idx][0].cpu().detach().numpy()
+          x_time = self.matched_eval_data[(self.matched_eval_data.time_idx>=decoder_time_idx-320) & (self.matched_eval_data.time_idx<decoder_time_idx.time_idx+32)]["time"]
+          logging.info(f"x_time:{x_time}")
+          fig = make_subplots(rows=2, cols=2)
+          fig.update_layout(
+              autosize=False, width=1500, height=800,
+          )
           prediction_date_time = str(self.data_table_ref.data[idx][5]) + "-" + str(self.data_table_ref.data[idx][6]) + "-" + str(self.data_table_ref.data[idx][7]) + " " + str(self.data_table_ref.data[idx][3]) + " " + str(self.data_table_ref.data[idx][4])
-          fig = self.pl_module.plot_prediction(x, out, idx=idx, add_loss_to_title=True)
-          # TODO: fix this so that we can get additional fig
-          if isinstance(fig, (list, tuple)):
-              fig = fig[0]
-          fig.update_layout(title=prediction_date_time)
+          fig.update_layout(title=prediction_date_time, font=dict(size=20))
+          #logging.info(f"self.data_table_ref.data[idx]:{self.data_table_ref.data[idx]}")
+          self.pl_module.plot_prediction(x, out, idx=idx, ax=fig, row=1, col=1, draw_cum=False, x_time=x_time)
+          self.pl_module.plot_prediction(x, out, idx=idx, ax=fig, row=2, col=1, draw_cum=True, x_time=x_time)
           img_bytes = fig.to_image(format="png") # kaleido library
           im = PIL.Image.open(BytesIO(img_bytes))
-          decoder_time_idx = x["decoder_time_idx"][idx][-1]
-          #logging.info(f"decoder_time_idx:{decoder_time_idx}")
+          #logging.info(f"decoder_time_idx:{x['decoder_time_idx'][idx]}")
           y_hat = y_hats[idx]
+          y_hat_cum = y_hats_cum[idx]
           y_raw = y_raws[idx]
           #logging.info(f"y_hat:{y_hat}")
           #logging.info(f"y_raw:{y_raw}")
@@ -242,7 +280,7 @@ class WandbClfEvalCallback(WandbEvalCallback, Callback):
           #im_interp = PIL.Image.open(BytesIO(img_bytes_interp))
           #img_interp = wandb.Image(im_interp)
 
-          preds.append([decoder_time_idx, torch.max(y_hat), torch.min(y_hat), img, rmse[idx], mae[idx]])
+          preds.append([decoder_time_idx, torch.max(y_hat_cum), torch.min(y_hat_cum), img, rmse[idx], mae[idx]])
       #logging.info(f"preds:{len(preds)}")
       return preds
      

src/pytorch_forecasting/data/timeseries.py
@@ -10,10 +10,21 @@ import inspect
 from typing import Any, Callable, Dict, List, Tuple, Union
 import warnings
 import logging
+import math
 import traceback
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from pytorch_forecasting.data.encoders import (
+    EncoderNormalizer,
+    GroupNormalizer,
+    MultiNormalizer,
+    NaNLabelEncoder,
+    TorchNormalizer,
+)
+from pytorch_forecasting.data.samplers import TimeSynchronizedBatchSampler
+from pytorch_forecasting.utils import repr_class
 from scipy.signal import argrelmax,argrelmin, argrelextrema, find_peaks
 from sklearn.exceptions import NotFittedError
 from sklearn.preprocessing import RobustScaler, StandardScaler
@@ -24,15 +35,6 @@ from torch.nn.utils import rnn
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.data.sampler import Sampler, SequentialSampler
 
-from pytorch_forecasting.data.encoders import (
-    EncoderNormalizer,
-    GroupNormalizer,
-    MultiNormalizer,
-    NaNLabelEncoder,
-    TorchNormalizer,
-)
-from pytorch_forecasting.data.samplers import TimeSynchronizedBatchSampler
-from pytorch_forecasting.utils import repr_class
 
 
 def _find_end_indices(diffs: np.ndarray, max_lengths: np.ndarray, min_length: int) -> Tuple[np.ndarray, np.ndarray]:
@@ -419,10 +421,12 @@ class TimeSeriesDataSet(Dataset):
         #df_index_high_39 = g['close_back'].transform(add_highs, width=13*3)
         #df_index_low_39 = g['close_back'].transform(add_lows, width=13*3)
         # reduce return to bring down loss
+        data['close_back'] = data['close_back'].apply(lambda x:math.log(1+x))
+        data['volume_back'] = data['volume_back'].apply(lambda x:math.log(1+x))
         if "ticker" in data.columns:
           data['close_back_cumsum'] = data.groupby(['ticker'])['close_back'].cumsum()
           # 1000 is required to bring down loss to avoid nan
-          data['volume_back_cumsum'] = data.groupby(['ticker'])['volume_back'].cumsum()/1000
+          data['volume_back_cumsum'] = data.groupby(['ticker'])['volume_back'].cumsum()
         #logging.info(f"df_index_low_13:{df_index_low_13}")
         #data["high_13"] = df_index_high_13
         #data["low_13"] = df_index_low_13

src/pytorch_forecasting/models/base_model.py
@@ -2,46 +2,31 @@
 Timeseries models share a number of common characteristics. This module implements these in a common base class.
 """
 from collections import namedtuple
-#import pytorch_lightning as pl
 import copy
 from copy import deepcopy
 import inspect
 import logging
 import os
-import PIL
 from io import BytesIO
 from typing import Any, Callable, Dict, Iterable, List, Literal, Optional, Tuple, Union
 import warnings
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
-import numpy
-import wandb
 import traceback
+
 import lightning.pytorch as pl
 from lightning.pytorch import LightningModule, Trainer
 from lightning.pytorch.callbacks import BasePredictionWriter, LearningRateFinder
 from lightning.pytorch.trainer.states import RunningStage
 from lightning.pytorch.utilities.parsing import AttributeDict, get_init_args
-from pytorch_forecasting.utils import create_mask, unpack_sequence, unsqueeze_like
-#from pytorch_lightning import LightningModule, Trainer
-#from pytorch_lightning.callbacks import BasePredictionWriter, LearningRateFinder
-#from pytorch_lightning.trainer.states import RunningStage
-#from pytorch_lightning.utilities.parsing import AttributeDict, get_init_args
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.lib.function_base import iterable
 import pandas as pd
+import PIL
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 import pytorch_optimizer
 from pytorch_optimizer import Ranger21
-import scipy.stats
-import torch
-import torch.nn as nn
-from torch.nn.utils import rnn
-from torch.optim.lr_scheduler import LambdaLR, ReduceLROnPlateau
-from torch.utils.data import DataLoader
-from tqdm.autonotebook import tqdm
-import yaml
-
+from pytorch_forecasting.utils import create_mask, unpack_sequence, unsqueeze_like
 from pytorch_forecasting.data import TimeSeriesDataSet
 from pytorch_forecasting.data.encoders import EncoderNormalizer, GroupNormalizer, MultiNormalizer, NaNLabelEncoder
 from pytorch_forecasting.metrics import (
@@ -68,6 +53,16 @@ from pytorch_forecasting.utils import (
     groupby_apply,
     to_list,
 )
+import scipy.stats
+import torch
+import torch.nn as nn
+from torch.nn.utils import rnn
+from torch.optim.lr_scheduler import LambdaLR, ReduceLROnPlateau
+from torch.utils.data import DataLoader
+from tqdm.autonotebook import tqdm
+import wandb
+import yaml
+
 
 # todo: compile models
 
@@ -775,7 +770,7 @@ class BaseModel(pl.LightningModule, InitialParameterRepresenterMixIn, TupleOutpu
             out = self(x, **kwargs)
             prediction = out["prediction"]
             #logger.info(f"x:{x}, out:{out}, y:{y}")
-            exit(0)
+            #exit(0)
             # handle multiple targets
             prediction_list = to_list(prediction)
             gradient = 0
@@ -1066,6 +1061,10 @@ class BaseModel(pl.LightningModule, InitialParameterRepresenterMixIn, TupleOutpu
         ax=None,
         quantiles_kwargs: Dict[str, Any] = {},
         prediction_kwargs: Dict[str, Any] = {},
+        row = 1,
+        col = 1,
+        draw_cum = False,
+        x_time = None
     ) -> plt.Figure:
         """
         Plot prediction of prediction vs actuals
@@ -1093,12 +1092,19 @@ class BaseModel(pl.LightningModule, InitialParameterRepresenterMixIn, TupleOutpu
         y_hats = to_list(self.to_prediction(out, **prediction_kwargs))
         y_quantiles = to_list(self.to_quantiles(out, **quantiles_kwargs))
 
+        fig = ax
+        if fig == None:
+            fig = make_subplots()
         # for each target, plot
         figs = []
         for y_raw, y_hat, y_quantile, encoder_target, decoder_target in zip(
             y_raws, y_hats, y_quantiles, encoder_targets, decoder_targets
         ):
             y_all = torch.cat([encoder_target[idx], decoder_target[idx]])
+            if draw_cum:
+                #logging.info(f"before cumsum: {y_all}")
+                y_all = torch.cumsum(y_all, dim=-1)
+                #logging.info(f"after cumsum: {y_all}")
             max_encoder_length = x["encoder_lengths"].max()
             y = torch.cat(
                 (
@@ -1106,11 +1112,28 @@ class BaseModel(pl.LightningModule, InitialParameterRepresenterMixIn, TupleOutpu
                     y_all[max_encoder_length : (max_encoder_length + x["decoder_lengths"][idx])],
                 ),
             )
+            #logging.info(f"y.shape:{y.shape}")
             # move predictions to cpu
             y_hat = y_hat.detach().cpu()[idx, : x["decoder_lengths"][idx]]
+            n_pred = y_hat.shape[0]
+            base = y[-n_pred-1].detach().cpu()
+            #logging.info(f"base:{base}")
             y_quantile = y_quantile.detach().cpu()[idx, : x["decoder_lengths"][idx]]
             y_raw = y_raw.detach().cpu()[idx, : x["decoder_lengths"][idx]]
-
+            if draw_cum:
+                #logging.info(f"y_raw before cumsum:{y_raw}, {y_raw.shape}")
+                #logging.info(f"y_hat before cumsum:{y_hat}, {y_hat.shape}")
+                #logging.info(f"y_quantile before cumsum:{y_quantile}, {y_quantile.shape}")
+                y_hat = torch.cumsum(y_hat, dim=-1)
+                y_hat = torch.add(y_hat, base)
+                y_raw = torch.cumsum(y_raw, dim=-1)
+                y_raw = torch.add(y_raw, base)
+                y_quantile = torch.cumsum(y_quantile, dim=-2)
+                y_quantile = torch.add(y_quantile, base)
+                #logging.info(f"y_raw:{y_raw}, {y_raw.shape}")
+                #logging.info(f"y_raw:{y_raw}, {yhat.shape}")
+                #logging.info(f"y_quantile:{y_quantile}, {y_quantile.shape}")
+                #logging.info(f"y_quantile after cumsum:{y_quantile}, {y_quantile.shape}")
             # move to cpu
             y = y.detach().cpu()
             # create figure
@@ -1118,86 +1141,55 @@ class BaseModel(pl.LightningModule, InitialParameterRepresenterMixIn, TupleOutpu
             #    fig, ax = plt.subplots()
             #else:
             #    fig = ax.get_figure()
-            fig = make_subplots()
-            fig.update_layout(
-                autosize=False,
-                width=2400,
-                height=1000,
-                margin=dict(l=20, r=20, t=20, b=20)
-            )
-            n_pred = y_hat.shape[0]
             max_context = 500
             x_start = min(max_context, y.shape[0] - n_pred)
-            x_obs = np.arange(-max_context, 0)
-            x_pred = np.arange(n_pred)
+            x_obs = np.arange(-x_start, 0)
+            #x_pred = np.arange(n_pred)
+            x_pred = x_time[-n_pred:]
             prop_cycle = iter(plt.rcParams["axes.prop_cycle"])
             obs_color = next(prop_cycle)["color"]
             pred_color = next(prop_cycle)["color"]
             # plot observed history
+            plotter = go.Scatter
             if len(x_obs) > 0:
-                if len(x_obs) > 1:
-                    #plotter = ax.plot
-                    plotter = go.Scatter
-                else:
-                    #plotter = ax.scatter
-                    plotter = go.Scatter
-                plot = plotter(x=x_obs, y=y[-max_context:-n_pred], name="observed", line=dict(color=obs_color))
-                fig.add_trace(plot)
-            if len(x_pred) > 1:
-                plotter = go.Scatter
-            else:
-                plotter = go.Scatter
+                #plot = plotter(x=x_obs, y=y[-max_context:-n_pred], name="observed" if draw_cum else None, line=dict(color=obs_color))
+                plot = plotter(x=x_time[-max_context:-n_pred], y=y[-max_context:-n_pred], name="observed" if draw_cum else None, line=dict(color=obs_color))
+                fig.add_trace(plot, row=row, col=col)
 
             # plot observed prediction
             if True:
             #if show_future_observed:
-                fig.add_trace(plotter(x=x_pred, y=y[-n_pred:], name="fut_observed", line=dict(color=obs_color)))
+                fig.add_trace(plotter(x=x_pred, y=y[-n_pred:], name="fut_observed" if draw_cum else None, line=dict(color=obs_color)), row=row, col=col)
 
             # plot prediction
-            fig.add_trace(plotter(x=x_pred, y=y_hat, name="predicted", line=dict(color=pred_color)))
+            fig.add_trace(plotter(x=x_pred, y=y_hat, name="predicted" if draw_cum else None, line=dict(color=pred_color)), row=row, col=col)
 
             # plot predicted quantiles
-            fig.add_trace(plotter(x=x_pred, y=y_quantile[:, y_quantile.shape[1] // 2], name="quantile mean", line=dict(color=pred_color)))
+            fig.add_trace(plotter(x=x_pred, y=y_quantile[:, y_quantile.shape[1] // 2], name="quantile mean" if draw_cum else None, line=dict(color=pred_color)), row=row, col=col)
             quantile_colors = ["red", "purple", "pink"]
             quantiles = [0.02, 0.1, 0.25]
             for i in range(y_quantile.shape[1] // 2):
                 if len(x_pred) > 1:
                     fig.add_trace(go.Scatter(x=x_pred, y=y_quantile[:, i],
-                                             fill='tonexty', mode='none', name=f"quantile {(1-quantiles[i]):.2f}",
-                                             fillcolor=quantile_colors[i]))
+                                             fill='tonexty', mode='none', name=f"quantile {(1-quantiles[i]):.2f}" if draw_cum else None,
+                                             fillcolor=quantile_colors[i]), row=row, col=col)
                     idx = y_quantile.shape[1]-(i+1)
-                    fig.add_trace(go.Scatter(x=x_pred, y=y_quantile[:, -i - 1], name=f"quantile {quantiles[i]:.2f}",
+                    fig.add_trace(go.Scatter(x=x_pred, y=y_quantile[:, -i - 1], name=f"quantile {quantiles[i]:.2f}" if draw_cum else None,
                                              fill='tonexty', # fill area between trace0 and trace1
-                                             mode='none', fillcolor=quantile_colors[i]))
+                                             mode='none', fillcolor=quantile_colors[i]), row=row, col=col)
                 else:
                     quantiles = torch.tensor([[y_quantile[0, i]], [y_quantile[0, -i - 1]]])
+                    logging.info(f"bad x_pred")
+                    exit(0)
                     fig.add_trace(go.errorbar(
                         x_pred,
                         y[[-n_pred]],
                         yerr=quantiles - y[-n_pred],
                         c=quantile_colors[i],
                         capsize=1.0,
-                    ))
+                        name="bad"
+                    ), row=row, col=col)
 
-            if add_loss_to_title is not False:
-                if isinstance(add_loss_to_title, bool):
-                    loss = self.loss
-                elif isinstance(add_loss_to_title, torch.Tensor):
-                    loss = add_loss_to_title.detach()[idx].item()
-                elif isinstance(add_loss_to_title, Metric):
-                    loss = add_loss_to_title
-                else:
-                    raise ValueError(f"add_loss_to_title '{add_loss_to_title}'' is unkown")
-                if isinstance(loss, MASE):
-                    loss_value = loss(y_raw[None], (y[-n_pred:][None], None), y[:n_pred][None])
-                elif isinstance(loss, Metric):
-                    try:
-                        loss_value = loss(y_raw[None], (y[-n_pred:][None], None))
-                    except Exception:
-                        loss_value = "-"
-                else:
-                    loss_value = loss
-                #go.set_title(f"Loss {loss_value}")
             #go.set_xlabel("Time index")
             figs.append(fig)
 