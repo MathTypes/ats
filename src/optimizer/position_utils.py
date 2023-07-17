dc47891389ec2fecde1619fcf8ff8578917a4c48
Jianjun Chen <jianjun00@gmail.com>
2023-07-17 22:02:51 +0000

    Add risk to objective

--- src/optimizer/position_utils.py
+++ src/optimizer/position_utils.py
@@ -2,29 +2,45 @@ import logging
 import cvxpy as cp
 import numpy as np
 
+
+def min_max_rets(returns_fcst):
+    cumsum = returns_fcst.cumsum()
+    return cumsum.min(), cumsum.max()
+
 class Optimizer(object):
-    def __init__(self, name: str, max_loss, gamma, initial_positions: None):
+    def __init__(self, name: str, max_loss,
+                 gamma = 0, sigma = 0, l_norm=1.5,
+                 pnl_risk_norm = 1.5,
+                 pnl_risk = 10,
+                 initial_positions = None):
         super().__init__()
         self.initial_positions = initial_positions
         self.max_loss = max_loss
         self.gamma = gamma
+        self.sigma = sigma
         self.n = initial_positions.shape[0]
-        self.l_norm = 0.5
+        self.l_norm = l_norm
+        self.pnl_risk_norm = pnl_risk_norm
+        self.pnl_risk = pnl_risk
         
-    def optimize(self, returns_fcst, quantile_returns_fcst):
+    def optimize(self, returns_fcst, min_neg_fcst, max_pos_fcst):
         w = cp.Variable(self.n)
         logging.info(f"returns_fcst:{returns_fcst}, w:{w}")
-        ret = np.dot(np.sum(returns_fcst), w)
+        cum_rets = np.sum(returns_fcst, axis=-1)
+        logging.info(f"cum_rets:{cum_rets}")
+        ret = cp.sum(cum_rets @ w)
         logging.info(f"ret:{ret}, w:{w}")
-        #buy_risk = np.dot(quantile_returns_fcst[:,:,0], w*maximum(w,0))
-        #sell_risk = np.dot(quantile_returns_fcst[:,:,1], w*minimum(w,0))
-        #risk = buy_risk + sell_risk
-        risk = 0
-        objective = cp.Maximize(ret - self.gamma * risk)
+        buy_risk = cp.sum(min_neg_fcst @ cp.maximum(w,0))
+        sell_risk = cp.sum(max_pos_fcst @ cp.minimum(w,0))
+        risk = buy_risk + sell_risk
+        logging.info(f"ret:{ret}, risk:{risk}")
+        objective = cp.Maximize(ret + self.sigma * risk)
+        #objective = cp.Maximize(ret)
         #wors_hour = cp.sum_smallest(np.dot(np.sum(quantile_returns_fcst), w))
 
         constraints = [
             cp.norm(w, self.l_norm) <= self.gamma,
+            cp.norm(risk, self.pnl_risk) <= self.pnl_risk_norm
         ]
 
         problem = cp.Problem(objective, constraints)
--- tests/optimizer/test_position_utils.py
+++ tests/optimizer/test_position_utils.py
@@ -5,7 +5,7 @@ import unittest.mock as mock
 import numpy as np
 import torch
 
-from optimizer.position_utils import Optimizer
+from optimizer.position_utils import Optimizer, min_max_rets
 from util import logging_utils
 
 @mock.patch("optimizer.position_utils.Optimizer")
@@ -24,14 +24,20 @@ def test_portfolio_opt():
     assert ret == 0
     logging.info(f"ret:{ret}")
 
+def test_max_pos_fcst():
+    ret_fcst = np.array([0, 1, 2, -1])
+    min_ret, max_ret = min_max_rets(ret_fcst)
+    assert math.isclose(min_ret, 0, rel_tol=0.01)
+    assert math.isclose(max_ret, 3, rel_tol=0.01)
+    
 def test_single_asset_gamma_zero():
     initial_positions = torch.tensor([0])
     optimizer = Optimizer(name="opt", max_loss=0, gamma=0,
                           initial_positions=initial_positions)
     returns_fcst = np.array([[0.1, 0.1, 0.1, 0.1]])
-    quantile_returns_fcst = np.array([[[0.1, 0.1, 0.1, 0.1],
-                                       [0.2, 0.2, 0.2, 0.2]]])
-    new_positions, ret, val = optimizer.optimize(returns_fcst, quantile_returns_fcst)
+    min_neg_fcst = np.array([-0.1])
+    max_pos_fcst = np.array([0.2])
+    new_positions, ret, val = optimizer.optimize(returns_fcst, min_neg_fcst, max_pos_fcst)
     assert math.isclose(new_positions, 0, rel_tol=0.01)
     #assert math.isclose(ret[0], 0, rel_tol=0.01)
     #assert math.isclose(val, 0, rel_tol=0.01)
@@ -41,12 +47,47 @@ def test_single_asset_gamma_two():
     optimizer = Optimizer(name="opt", max_loss=0, gamma=2,
                           initial_positions=initial_positions)
     returns_fcst = np.array([[0.1, 0.1, 0.1, 0.1]])
-    quantile_returns_fcst = np.array([[[0.1, 0.1, 0.1, 0.1],
-                                       [0.2, 0.2, 0.2, 0.2]]])
-    new_positions, ret, val  = optimizer.optimize(returns_fcst, quantile_returns_fcst)
+    min_neg_fcst = np.array([-0.1])
+    max_pos_fcst = np.array([0.2])
+    new_positions, ret, val = optimizer.optimize(returns_fcst,min_neg_fcst, max_pos_fcst)
     assert math.isclose(new_positions, 2, rel_tol=0.01)
     #assert math.isclose(ret[0], 0.8, rel_tol=0.01)
     #assert math.isclose(val, 0.8, rel_tol=0.01)
+
+def test_single_asset_large_neg_drawback():
+    initial_positions = torch.tensor([0])
+    optimizer = Optimizer(name="opt", max_loss=0, gamma=2, sigma=2,
+                          initial_positions=initial_positions)
+    returns_fcst = np.array([[0.1, -0.3, 0.2, 0.7]])
+    min_neg_fcst = np.array([-0.7])
+    max_pos_fcst = np.array([0.7])
+    new_positions, ret, val = optimizer.optimize(returns_fcst,min_neg_fcst, max_pos_fcst)
+    assert math.isclose(new_positions, 0, rel_tol=0.01)
+    #assert math.isclose(ret[0], 0.8, rel_tol=0.01)
+    #assert math.isclose(val, 0.8, rel_tol=0.01)
+
+def test_single_asset_small_neg_drawback():
+    initial_positions = torch.tensor([0])
+    optimizer = Optimizer(name="opt", max_loss=0, gamma=2, sigma=2,
+                          pnl_risk=10,
+                          initial_positions=initial_positions)
+    returns_fcst = np.array([[0.1, -0.3, 0.2, 0.7]])
+    min_neg_fcst = np.array([-0.5])
+    max_pos_fcst = np.array([0.7])
+    new_positions, ret, val = optimizer.optimize(returns_fcst,min_neg_fcst, max_pos_fcst)
+    assert math.isclose(new_positions, 2, rel_tol=0.01)
+
+def test_two_assets_gamma_two():
+    initial_positions = torch.tensor([0, 0])
+    optimizer = Optimizer(name="opt", max_loss=0, gamma=4,
+                          initial_positions=initial_positions)
+    returns_fcst = np.array([[0.1, 0.1, 0.1, 0.1], [0.0, 0.0, 0.0, 0.0]])
+    min_neg_fcst = np.array([-0.1, 0])
+    max_pos_fcst = np.array([0.2, 0])
+    new_positions, ret, val  = optimizer.optimize(returns_fcst, min_neg_fcst, max_pos_fcst)
+    logging.info(f"new_positions:{new_positions}")
+    assert math.isclose(new_positions[0], 4, rel_tol=0.01)
+    assert math.isclose(new_positions[1], 0, rel_tol=0.01)
 
 if __name__ == "__main__":
     logging_utils.init_logging()
