e21af50762ca295a7b8ad02fa2a1115515720a04
Jianjun Chen <jianjun00@gmail.com>
2023-07-21 02:35:56 +0000

    Add utils to manage market data

---
+++ src/ats/market_data/market_data_mgr.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+import datetime
+import logging
+
+import pandas as pd
+import pytz
+from pytorch_forecasting.utils import create_mask, detach, to_list
+import torch
+
+from ats.calendar import market_time
+from ats.model.data_module import (
+    TransformerDataModule,
+    LSTMDataModule,
+    TimeSeriesDataModule,
+)
+from ats.event.macro_indicator import MacroDataBuilder
+from ats.model.models import AttentionEmbeddingLSTM
+from ats.model import model_utils
+from ats.prediction import prediction_utils
+from ats.model.utils import Pipeline
+from ats.model import viz_utils
+from ats.optimizer import position_utils
+from ats.util.profile import profile
+from ats.calendar import market_time
+
+class MarketDataMgr(object):
+    def __init__(
+        self,
+        config,
+        market_cal,
+    ):
+        super().__init__()
+        self.config = config
+        self.market_cal = market_cal
+        self.macro_data_builder = MacroDataBuilder()
+
+    
