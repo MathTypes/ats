f93022cf5e3dd36e64b5bfb61a319d30b44d8ac6
Jianjun Chen <jianjun00@gmail.com>
2023-07-21 05:04:08 +0000

    Read from monthly file

--- src/ats/event/macro_indicator.py
+++ src/ats/event/macro_indicator.py
@@ -1,118 +1,34 @@
+import datetime
 import logging
 
 import requests
 import pandas as pd
 
+from ats.calendar import date_utils
 from ats.util import logging_utils
 
 class MacroDataBuilder:
-    def __init__(
-        self,
-        api_key: str = "5e7a5f80ba03257b5913e97bec786466",
-        base_url: str = "https://api.stlouisfed.org/fred/series/observations?series_id=",
-    ):
-        self.api_key = api_key
-        self.base_url = base_url
-        observations = pd.read_csv(
-            "data/event/macro/series_observation/2023-07/2000-01-01_2023-07-20.csv",
-            header="~")
-        self.observations = observations[["date","value","series_id"]]
+    def __init__(self, config):
+        self.config = config
+        start_date = datetime.datetime.strptime(config.job.train_start_date, "%Y-%m-%d")
+        end_date = datetime.datetime.strptime(config.job.test_end_date, "%Y-%m-%d")
+        df_vec = []
+        for begin, end in date_utils.monthlist(start_date, end_date):
+            year_month_str = begin.strftime("%Y-%m")
+            date_str = begin.strftime("%Y-%m-%d")
+            observations = pd.read_csv(
+                f"/home/ubuntu/ats/data/event/macro/series_observation/{year_month_str}/{date_str}.csv",
+                sep="~")
+            observations.date = observations.date.apply(lambda x:datetime.datetime.strptime(x, "%Y-%m-%d"))
+            logging.info(f"observations:{observations.iloc[-3:]}")
+            observations["event_time"] = observations.date.apply(
+                lambda x: datetime.datetime.combine(x, datetime.datetime.min.time()).timestamp())
+        observations = pd.concat(df_vec)
+        self.observations = observations[["date","value","series_id", "event_time"]]
+        logging.info(f"observations:{self.observations.iloc[-3:]}")
 
     def has_event(self, dt):
         df = self.observations[(self.observations.event_time<dt.timestamp()) &
                                (self.observations.event_time>dt.timestamp()-86400000)]
         return not df.empty
 
-    def most_recent_fetch(self, api_key: str, base_url: str):
-        # FRED Series IDs for each economic indicator
-        series_ids = {
-            "GDP": "GDP",
-            "Unemployment Rate": "UNRATE",
-            "Inflation Rate": "T10YIE",  # 10-Year Breakeven Inflation Rate
-            "Interest Rates": "GS10",  # 10-Year Treasury Constant Maturity Rate
-            "Housing Market Indicator": "HOUST",  # Housing Starts: Total: New Privately Owned Housing Units Started
-        }
-
-        rows = []
-
-        for name, series_id in series_ids.items():
-            url = f"{base_url}{series_id}&api_key={api_key}&file_type=json"
-            response = requests.get(url).json()
-            data = response["observations"]
-            df = pd.DataFrame(data)
-            df["date"] = pd.to_datetime(df["date"])
-            most_recent_data = df.loc[df["date"].idxmax()]
-            print(name)
-            rows.append(
-                {
-                    "date": most_recent_data["date"],
-                    "indicator": name,
-                    "indicator_value": most_recent_data["value"],
-                }
-            )
-
-        return pd.DataFrame(rows)
-
-    def full_fetch(self, api_key: str, base_url: str):
-        # FRED Series IDs for each economic indicator
-        series_ids = {
-            "GDP": "GDP",
-            "Unemployment Rate": "UNRATE",
-            "Inflation Rate": "T10YIE",  # 10-Year Breakeven Inflation Rate
-            "Interest Rates": "GS10",  # 10-Year Treasury Constant Maturity Rate
-            "Housing Market Indicator": "HOUST",  # Housing Starts: Total: New Privately Owned Housing Units Started
-        }
-
-        rows = []
-
-        for name, series_id in series_ids.items():
-            url = f"{base_url}{series_id}&api_key={api_key}&file_type=json"
-            response = requests.get(url).json()
-
-            data = response["observations"]
-            df = pd.DataFrame(data)
-
-            df["date"] = pd.to_datetime(df["date"])
-
-            rows.append(
-                {"date": df["date"], "indicator": name, "indicator_value": df["value"]}
-            )
-
-        return pd.DataFrame(rows)
-
-
-    def add_macro_events(series_ids):
-        df = self.observations[self.observations.series_id.isin(series_ids)]
-        df["date"] = pd.to_datetime(df["date"])
-        df = df.pivot_table(
-            index="date", columns="series_id", values="value", aggfunc="first"
-        )
-        df.reset_index(inplace=True)
-        df["event_time"] = df.date.apply(lambda x: datetime.combine(x, datetime.min.time()))
-        logging.info(f"df:{df}")
-        return df
-
-def add_macro_indicators(api_key, base_url):
-    macro_information = MacroDataBuilder(api_key, base_url)
-    macro_data = macro_information.full_fetch(api_key, base_url)
-    return macro_data
-
-
-if __name__ == "__main__":
-    logging_utils.init_logging()
-    pd.set_option("display.max_columns", None)
-    pd.set_option("display.max_rows", None)
-    indicators_df = add_macro_indicators(api_key="5e7a5f80ba03257b5913e97bec786466",
-                                         base_url="https://api.stlouisfed.org/fred/series/observations?series_id=")
-    series_names = pd.read_csv("data/event/macro/fred_series_names.txt")
-    for idx, row in series_names.iterrows():
-    logging.info(f"indicators_df:{indicators_df}")
-    indicators_df = indicators_df.apply(lambda x: x.explode()).reset_index(drop=True)
-    logging.info(f"indicators_df_explode:{indicators_df}")
-    indicators_df["date"] = pd.to_datetime(indicators_df["date"])
-    indicators_df_pivot = indicators_df.pivot_table(
-        index="date", columns="indicator", values="indicator_value", aggfunc="first"
-    )
-    logging.info(f"pivot:{indicators_df}")
-    indicators_df_pivot.reset_index(inplace=True)
-    #    data_df = pd.merge(data_df, indicators_df_pivot, on='date', how='left')
