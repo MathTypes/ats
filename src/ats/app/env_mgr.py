5d6977e0e6bad0225d0bc4746aa73f621306f536
Jianjun Chen <jianjun00@gmail.com>
2023-07-23 16:59:23 +0000

    Set context length and prediction length

--- src/ats/app/env_mgr.py
+++ src/ats/app/env_mgr.py
@@ -18,61 +18,68 @@ class EnvMgr(object):
 
     def init_env(self):
         self.market_cal = mcal.get_calendar(self.config.job.market)
-        self.train_start_date = datetime.datetime.strptime(
-            self.config.job.train_start_date, "%Y-%m-%d"
-        ).replace(tzinfo=datetime.timezone.utc)
-        self.train_start_timestamp = market_time.get_open_time(
-            self.market_cal, self.train_start_date
-        )
-        self.eval_start_date = datetime.datetime.strptime(
-            self.config.job.eval_start_date, "%Y-%m-%d"
-        ).replace(tzinfo=datetime.timezone.utc)
-        self.eval_start_timestamp = market_time.get_open_time(
-            self.market_cal, self.eval_start_date
-        )
-        self.eval_end_date = datetime.datetime.strptime(
-            self.config.job.eval_end_date, "%Y-%m-%d"
-        ).replace(tzinfo=datetime.timezone.utc)
-        self.test_start_date = datetime.datetime.strptime(
-            self.config.job.test_start_date, "%Y-%m-%d"
-        ).replace(tzinfo=datetime.timezone.utc)
-        self.eval_end_timestamp = market_time.get_close_time(
-            self.market_cal, self.eval_end_date
-	)
-        self.test_start_timestamp = market_time.get_open_time(
-            self.market_cal, self.test_start_date
-        )
-        self.test_end_date = datetime.datetime.strptime(
-            self.config.job.test_end_date, "%Y-%m-%d"
-        ).replace(tzinfo=datetime.timezone.utc)
-        self.test_end_timestamp = market_time.get_close_time(
-            self.market_cal, self.test_end_date
-        )
-        self.max_lags = self.config.job.max_lags
-        start_date = None
-        if self.config.job.mode == "train":
-            self.train_start_date = datetime.datetime.strptime(
-                self.config.job.train_start_date, "%Y-%m-%d"
+        has_train_stage = self.config.job.mode in ["train", "test"]
+        has_eval_stage = self.config.job.mode in ["train", "eval"]
+        has_test_stage = self.config.job.mode in ["test"]
+
+        if has_eval_stage:
+            self.eval_start_date = datetime.datetime.strptime(
+                self.config.job.eval_start_date, "%Y-%m-%d"
             ).replace(tzinfo=datetime.timezone.utc)
-        elif self.config.job.mode == "test":
+            self.eval_start_timestamp = market_time.get_open_time(
+                self.market_cal, self.eval_start_date
+            )
+            self.eval_end_date = datetime.datetime.strptime(
+                self.config.job.eval_end_date, "%Y-%m-%d"
+            ).replace(tzinfo=datetime.timezone.utc)
+
+        if has_test_stage:
+            self.test_start_date = datetime.datetime.strptime(
+                self.config.job.test_start_date, "%Y-%m-%d"
+            ).replace(tzinfo=datetime.timezone.utc)
+            self.eval_end_timestamp = market_time.get_close_time(
+                self.market_cal, self.eval_end_date
+	    )
+            self.test_start_timestamp = market_time.get_open_time(
+                self.market_cal, self.test_start_date
+            )
+            self.test_end_date = datetime.datetime.strptime(
+                self.config.job.test_end_date, "%Y-%m-%d"
+            ).replace(tzinfo=datetime.timezone.utc)
+            self.test_end_timestamp = market_time.get_close_time(
+                self.market_cal, self.test_end_date
+            )
             self.data_start_date = self.test_start_date - datetime.timedelta(
                 days=self.max_lags
             )
-        logging.info(
-            f"train_start_date:{self.train_start_date}, test_start_date:{self.test_start_date}, test_end_date:{self.test_end_date}"
-        )
-        start_date = self.train_start_date
-        if not start_date:
-            start_date = self.data_start_date
-        self.start_date = start_date
-        self.end_date = self.test_end_date
+        self.max_lags = self.config.job.max_lag
+        if has_train_stage:
+            self.train_start_date = datetime.datetime.strptime(
+                self.config.job.train_start_date, "%Y-%m-%d"
+            ).replace(tzinfo=datetime.timezone.utc)
+            self.train_start_timestamp = market_time.get_open_time(
+                self.market_cal, self.train_start_date
+            )
+        data_start_date = None
+        data_end_date = None
+        if self.config.job.mode == "train":
+            data_start_date = self.train_start_date
+            data_end_date = self.test_end_date
+        elif self.config.job.mode == "test":
+            data_start_date = self.train_start_date
+            data_end_date = self.test_end_date
+        elif self.config.job.mode == "eval":
+            data_start_date = self.eval_start_date
+            data_end_date = self.eval_end_date
+        self.data_start_date = data_start_date - datetime.timedelta(days=config.job.max_lag)
+        self.data_end_date = data_end_date + datetime.timedelta(days=config.job.max_lead)
         self.dataset_base_dir = self.config.dataset.base_dir
         self.model_tickers = self.config.dataset.model_tickers
         self.heads, self.targets = model_utils.get_heads_and_targets(self.config)
         self.time_interval = self.config.dataset.time_interval
         self.target_size = len(self.targets) if isinstance(self.targets, List) else 1
-        self.data_start_timestamp = self.train_start_timestamp
-        self.data_end_timestamp = self.test_end_timestamp
+        self.context_length = self.config.model.context_length
+        self.prediction_length = self.config.model.prediction_length
         logging.info(
-            f"start_date:{start_date}, test_start_date:{self.test_start_date}, test_end_date:{self.test_end_date}"
+            f"data_start_date:{self.data_start_date}, data_end_date:{self.data_end_date}"
         )
