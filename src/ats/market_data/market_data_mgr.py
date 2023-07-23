744002ba12585cadfd93661aaf2232910356c456
Jianjun Chen <jianjun00@gmail.com>
2023-07-23 20:44:00 +0000

    Use yyyymmdd for snapshot dir

--- src/ats/market_data/market_data_mgr.py
+++ src/ats/market_data/market_data_mgr.py
@@ -120,7 +120,9 @@ class MarketDataMgr(object):
     def _get_snapshot(self):
         raw_data = None
         env_mgr = self.env_mgr
-        snapshot_dir = f"{self.config.dataset.snapshot}/{env_mgr.data_start_date}_{env_mgr.data_end_date}"
+        data_start_date_str = env_mgr.data_start_date.strftime('%Y%m%d')
+        data_end_date_str = env_mgr.data_end_date.strftime('%Y%m%d')
+        snapshot_dir = f"{self.config.dataset.snapshot}/{data_start_date_str}_{data_end_date_str}"
         try:
             if self.config.dataset.read_snapshot and os.listdir(f"{snapshot_dir}"):
                 logging.info(f"reading snapshot from {snapshot_dir}")
