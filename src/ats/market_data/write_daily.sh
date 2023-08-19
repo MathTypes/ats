c457723aa76fd1ab29f2bce91b50867f0c54d358
Jianjun Chen <jianjun00@gmail.com>
2023-08-19 03:34:42 +0000

    Add shell scripts for writing data

---
+++ src/ats/market_data/write_daily.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Example: sh -x src/ats/market_data/write_month.sh 2023-05-01 $PWD/data/firstratedata/full
+base_dir=$(dirname "$0")
+root_dir=$base_dir/../../..
+echo $root_dir
+tickers=$(cat "$base_dir/firstratedata_fut.txt")
+export RAY_DATA_STRICT_MODE=0
+for ticker in $tickers
+do
+    poetry run python3 src/ats/market_data/write_monthly_ts.py --ticker=$ticker --asset_type=FUT --start_date=2008-01-01 \
+	   --freq=1d --input_dir=data/firstratedata/full --output_dir=$root_dir/data
+done
