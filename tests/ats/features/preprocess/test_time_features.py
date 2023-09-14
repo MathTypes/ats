cb042b7eea203a26d4ba7ea8010a296cc2db7613
Jianjun Chen <jianjun00@gmail.com>
2023-09-14 02:30:47 +0000

    Add test for new_york_open feature

--- tests/ats/features/preprocess/test_time_features.py
+++ tests/ats/features/preprocess/test_time_features.py
@@ -6,13 +6,16 @@ from hamilton import base, driver, log_setup
 from hamilton.experimental import h_ray
 from hamilton.experimental import h_cache
 from hydra import initialize, compose
+import ray
 from ray import workflow
 
 from ats.app.env_mgr import EnvMgr
 from ats.market_data import market_data_mgr
+from ats.util import logging_utils
 
 # THe config path is relative to the file calling initialize (this file)
-def run_features(feature_name):
+def run_features(feature_name, k=10):
+    logging_utils.init_logging()
     pd.set_option("display.max_columns", None)
     pd.set_option("display.max_rows", None)
     pd.options.display.float_format = "{:,.4f}".format
@@ -27,7 +30,10 @@ def run_features(feature_name):
         )
         env_mgr = EnvMgr(cfg)
         md_mgr = market_data_mgr.MarketDataMgr(env_mgr)
-        log_setup.setup_logging()
+        #log_setup.setup_logging()
+        ray.init(object_store_memory=30*1024*1024*1024,
+                 storage=f"{cfg.dataset.base_dir}/cache",
+                 log_to_driver=True)
         workflow.init()
         # You can also script module import loading by knowing the module name
         # See run.py for an example of doing it that way.
@@ -53,14 +59,34 @@ def run_features(feature_name):
             workflow_id=f"wf-{env_mgr.run_id}",
         )
         dr = driver.Driver(initial_columns, *modules, adapter=rga)
-        full_data = dr.execute([feature_name])[-10:]
+        logging.error(f"feature_name:{feature_name}")
+        full_data = dr.execute([feature_name])[-k:]
         return full_data
     
-def test_time_low():
+def test_time_low_21d():
     full_data = run_features("time_low_21d_ff")
     print(f"full_data:{full_data}")
     np.testing.assert_array_almost_equal(
         full_data["timestamp"],
+        [1283252400, 1283252400, 1283252400, 1283252400, 1283252400,
+         1283252400, 1283252400, 1283252400, 1283252400, 1283252400],
+        decimal=3
+    )
+
+def test_new_york_close_time():
+    result = run_features("new_york_close_time", 5)
+    print(f"result:{result}")
+    np.testing.assert_array_almost_equal(
+        result["new_york_close_time"],
+        [1283544000, 1283544000, 1283544000, 1283544000, 1283544000],
+        decimal=3
+    )
+    
+def test_time_low_5():
+    result = run_features("time_low_5_ff")
+    print(f"result:{result}")
+    np.testing.assert_array_almost_equal(
+        result["timestamp"],
         [1283252400, 1283252400, 1283252400, 1283252400, 1283252400,
          1283252400, 1283252400, 1283252400, 1283252400, 1283252400],
         decimal=3
