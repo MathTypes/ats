4fc3f0f67499f00fe99f83b27a142c765f6a062e
Jianjun Chen <jianjun00@gmail.com>
2023-10-09 00:19:54 +0000

    Remove test that should not be part of pnl tests

--- tests/ats/trading/test_pnl.py
+++ tests/ats/trading/test_pnl.py
@@ -29,35 +29,3 @@ pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 pd.options.display.float_format = "{:.5f}".format
 
-def test_add_highs_trending_no_high():
-    start_timestamp = 1325689200
-    delta = 30*60
-    timestamps = [start_timestamp + i*delta for i in range(7)]
-    raw_data = {
-        "ticker": ["ES", "ES", "ES", "ES", "ES", "ES", "ES"],
-        "open": [1, 2, 3, 4, 5, 6, 7],
-        "high": [3, 4, 5, 6, 7, 8, 9],
-        "low": [1, 2, 3, 4, 5, 6, 7],
-        "close": [1, 2, 3, 4, 5, 6, 7],
-        "volume": [1, 3, 2, 1, 2, 3, 4],
-        "dv": [1, 2, 3, 1, 2, 3, 1],
-        "timestamp": timestamps
-    }
-    raw_data = pd.DataFrame(data=raw_data)
-    full_ds = ray.data.from_pandas(raw_data)
-    add_group_features = partial(data_util.add_group_features, 30)
-    full_ds = full_ds.groupby("ticker").map_groups(add_group_features)
-    raw_data = full_ds.to_pandas()
-    row_two = raw_data.iloc[2]
-    assert row_two["ticker"] == "ES"
-    assert row_two["close"] == 3
-    assert row_two["volume"] == 2
-    assert row_two["dv"] == 3
-    assert row_two["cum_volume"] == 6
-    assert row_two["cum_dv"] == 6
-    assert math.isclose(row_two["close_back"], 0.0019900504080103687, rel_tol=0.01)
-    assert math.isclose(row_two["volume_back"], -0.2231435513142097, rel_tol=0.01) # 3=>2
-    assert pd.isna(row_two["close_high_5_ff"])
-    assert pd.isna(row_two["time_high_5_ff"])
-    assert pd.isna(row_two["close_low_5_ff"])
-    assert pd.isna(row_two["time_low_5_ff"])
